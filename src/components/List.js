b4a0b9fa281b8aad48b9727851c7255568efe082
ryt6758 <tyr0412utx.dall@gmail.com>
2020-03-12 13:37:37 +0900

    create, delete action実装

--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import { del, update } from '../actions';
 
 class List extends Component {
-
+  deletePost(e) {
+    const targetDeleteDOM = e.target.parentNode;
+    const deletePostId = targetDeleteDOM.dataset.postId;
+    return (deletePostId);
+  }
   render() {
-
-    const todos = this.props.todoList.map(todo => {
+    const todos = _.map(this.props.todoList, todo => {
       return (
-        <React.Fragment>
-          <div>{todo.title}</div>
-          <div>{todo.body}</div>
-        </React.Fragment>
+        <div data-post-id={todo.id} key={todo.id}>
+          <label htmlFor="todoTitle">Title：</label><input defaultValue={todo.title} id="todoTitle" />
+          <label htmlFor="todoBody">Body：</label><input defaultValue={todo.body} id="todoBody" />
+          <button onClick={(e) => { this.props.delete(this.deletePost(e)); }}>del</button>
+        </div>
       )
     })
     return (
-      <div>{todos}</div>
+      <React.Fragment>{todos}</React.Fragment>
     )
   }
-
 }
 
 const mapStateToProps = state => ({
   todoList: state.todoList
 });
 
+const mapDispatchToProps = dispatch => ({
+  delete: postId => dispatch(del(postId)),
+  update: value => dispatch(update(value))
+})
 
-
-export default connect(mapStateToProps, null)(List);
+export default connect(mapStateToProps, mapDispatchToProps)(List);
